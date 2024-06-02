62d4d32b92d4da5a41433bd9571a4d1c8c351bb9
Jesus Ayala <jesusayala1996cult@gmail.com>
2024-06-02 16:05:44 -0500

    fix: deploy

--- config/database.py
+++ config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from config.config_general import settings  # Importar la configuración
+from config.config_general import settings
 
 
 
